f9e627cbecb5d8782dea4d3a368d325f6b726f8e
alexey.svetlenko <alexey.svetlenko@gmail.com>
2016-09-22 17:52:41 +0300

    start up Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,80 +1,8 @@
 import {Component} from '@angular/core';
+import { OnInit } from '@angular/core'
 
-import {Hero} from './hero'
-
-
-const HEROES: Hero[] = [
-    {
-        id: 11,
-        firstName: 'John',
-        lastName: 'Tolkien',
-        email: 'tolkien@inklings.com',
-        phone: '867-5309'
-    },
-    {
-        id: 12,
-        firstName: 'Clive',
-        lastName: 'Lewis',
-        email: 'lewis@inklings.com',
-        phone: '867-5309'
-    },
-    {
-        id: 13,
-        firstName: 'Owen',
-        lastName: 'Barfield',
-        email: 'barfield@inklings.com',
-        phone: '867-5309'
-    },
-    {
-        id: 14,
-        firstName: 'Charles',
-        lastName: 'Williams',
-        email: 'williams@inklings.com',
-        phone: '867-5309'
-    },
-    {
-        id: 15,
-        firstName: 'Roger',
-        lastName: 'Green',
-        email: 'green@inklings.com',
-        phone: '867-5309'
-    },
-    {
-        id: 16,
-        firstName: 'Carl',
-        lastName: 'Brisly',
-        email: 'carloso.brisly@gmail.com',
-        phone: '555-84754'
-    },
-    {
-        id: 17,
-        firstName: 'Peruano',
-        lastName: 'Mondelo',
-        email: 'p.mondelo@gmail.com',
-        phone: '758-95412'
-    },
-    {
-        id: 18,
-        firstName: 'Durandal',
-        lastName: 'Freime',
-        email: 'f.durandal@torento.com',
-        phone: '844-44874'
-    },
-    {
-        id: 19,
-        firstName: 'Alexand',
-        lastName: 'Orlov',
-        email: 'alexandr.orlov@mail.ru',
-        phone: '977-87459'
-    },
-    {
-        id: 20,
-        firstName: 'Vasiliy',
-        lastName: 'Petrov',
-        email: 'petia.vasichkin@mumu.com',
-        phone: '111-77788'
-    }
-];
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -135,14 +63,27 @@ const HEROES: Hero[] = [
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+    providers: [HeroService]
 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService){
+
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void{
+        this.getHeroes();
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
---
+++ app/mock-heroes.ts
@@ -0,0 +1,74 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[] = [
+    {
+        id: 11,
+        firstName: 'John',
+        lastName: 'Tolkien',
+        email: 'tolkien@inklings.com',
+        phone: '867-5309'
+    },
+    {
+        id: 12,
+        firstName: 'Clive',
+        lastName: 'Lewis',
+        email: 'lewis@inklings.com',
+        phone: '867-5309'
+    },
+    {
+        id: 13,
+        firstName: 'Owen',
+        lastName: 'Barfield',
+        email: 'barfield@inklings.com',
+        phone: '867-5309'
+    },
+    {
+        id: 14,
+        firstName: 'Charles',
+        lastName: 'Williams',
+        email: 'williams@inklings.com',
+        phone: '867-5309'
+    },
+    {
+        id: 15,
+        firstName: 'Roger',
+        lastName: 'Green',
+        email: 'green@inklings.com',
+        phone: '867-5309'
+    },
+    {
+        id: 16,
+        firstName: 'Carl',
+        lastName: 'Brisly',
+        email: 'carloso.brisly@gmail.com',
+        phone: '555-84754'
+    },
+    {
+        id: 17,
+        firstName: 'Peruano',
+        lastName: 'Mondelo',
+        email: 'p.mondelo@gmail.com',
+        phone: '758-95412'
+    },
+    {
+        id: 18,
+        firstName: 'Durandal',
+        lastName: 'Freime',
+        email: 'f.durandal@torento.com',
+        phone: '844-44874'
+    },
+    {
+        id: 19,
+        firstName: 'Alexand',
+        lastName: 'Orlov',
+        email: 'alexandr.orlov@mail.ru',
+        phone: '977-87459'
+    },
+    {
+        id: 20,
+        firstName: 'Vasiliy',
+        lastName: 'Petrov',
+        email: 'petia.vasichkin@mumu.com',
+        phone: '111-77788'
+    }
+];
