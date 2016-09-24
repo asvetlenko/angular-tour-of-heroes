596f55b9cbfa186fbb623aa40dd70c855bc3f3f3
alexey <asvetlenko@gmail.com>
2016-09-24 12:45:19 +0300

    move app.modudle to directory modules

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -1,4 +1,4 @@
-import './common/rxjs-extensions';
+import '../common/rxjs-extensions';
 
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
@@ -6,18 +6,18 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
-import {InMemoryDataService} from './_fake-server/in-memory-data.service';
+import {InMemoryDataService} from './../_fake-server/in-memory-data.service';
 
-import {AppComponent} from './components/app/app.component'
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {HeroesComponent} from './components/heroes/heroes.component';
-import {HeroDetailComponent} from './components/hero-detail/hero-detail.component';
-import {HeroSearchComponent} from './components/hero-search/hero-search.component';
-import {TooltipHeroComponent} from './components/tooltip-hero/tooltip-hero-component';
+import {AppComponent} from './../components/app/app.component'
+import {DashboardComponent} from './../components/dashboard/dashboard.component';
+import {HeroesComponent} from './../components/heroes/heroes.component';
+import {HeroDetailComponent} from './../components/hero-detail/hero-detail.component';
+import {HeroSearchComponent} from './../components/hero-search/hero-search.component';
+import {TooltipHeroComponent} from './../components/tooltip-hero/tooltip-hero-component';
 
-import {HeroService} from './web-services/hero.service';
+import {HeroService} from './../web-services/hero.service';
 
-import {routing} from './routings/app.routing'
+import {routing} from './../routings/app.routing'
 
 @NgModule({
     imports: [
